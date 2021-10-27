a5848323e615b0e480f8a8eaf63770401dee6451
JengaMasterG <grsruggirello25@gmail.com>
2021-10-27 13:56:58 -0400

    Updated Binary Search

--- main.py
+++ main.py
@@ -12,41 +12,55 @@ random.seed(0)
 #---Running Merge Sort---#
 print("---Running Merge Sort Algorithm----")
 
-B = [random.randint(0,10**7) for i in range(2**20)] #generates random numbered list to 2 to 20th
+mergeArray = [random.randint(0,10**7) for i in range(2**20)] #generates random numbered list to 2 to 20th
 
-print (B[0:5]) #prints inital order of first 5 values
-print (B[10000:10005]) #prins inital order of values 10000 to 10005
+print (mergeArray[0:5]) #prints inital order of first 5 values
+print (mergeArray[10000:10005]) #prins inital order of values 10000 to 10005
 
-MergeSort(B) #runs merge sort algorithm
+MergeSort(mergeArray) #runs merge sort algorithm
 
-print (B[0:5]) #should be first 5 smallest values
-print (B[10000:10005]) #should be 5 different values
+print (mergeArray[0:5]) #should be first 5 smallest values
+print (mergeArray[10000:10005]) #should be 5 different values
 
 #---Running Binary Search---#
 print("---Running Binary Search Algorithm---")
 
-array_100 = [random.randint(10**10) for i in range(0,100)] #generates random numbered list to 100
-
-print(array_100[1])
+array_100 = [random.randint(0, 10**10) for i in range(0,100)] #generates random numbered list to 100
 
 MergeSort(array_100) #Sort array
 
 #Grab Random Integers for value finding. 2 for array, 1 not in array
 rand1 = random.randint(0,100)
 rand2 = random.randint(0,100)
-rand3 = random.randint(0, 10**10)
-
-print(rand1, rand2, rand3)
-
-#Grab Random Integers for value finding. 2 for array, 1 not in array
-rand1 = random.randint(0,100)
-rand2 = random.randint(0,100)
 rand3 = random.randint(0,10**10)
 
-print(rand1, rand2, rand3)
-
+#Set Values for Find Variables from Random Variables
 find1 = array_100[rand1]
 find2 = array_100[rand2]
 find3 = rand3
 
-BinarySearch(array_100, find1, find2,find3)
+print("Searching for {}, {}, and {}...".format(find1, find2, find3))
+
+#Find Values via Binary Search
+found1 = BinarySearch(array_100, 0, len(array_100) - 1, find1)
+found2 = BinarySearch(array_100, 0, len(array_100) - 1, find2)
+found3 = BinarySearch(array_100, 0, len(array_100) - 1, find3)
+
+#Print Results
+if found1 is None:
+    print("{} does not exist in Array".format(find1))
+    
+else:
+    print("{} exists in Array".format(find1))
+
+if found2 is None:
+    print("{} does not exist in Array".format(find2))
+    
+else:
+    print("{} exists in Array".format(find2))
+    
+if found3 is None:
+    print("{} does not exist in Array".format(find3))
+    
+else:
+    print("{} exists in Array".format(find3))
