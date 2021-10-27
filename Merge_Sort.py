0fb4462bbfa89f8fb362eb757aaeac85da6778fe
Giovanni Ruggirello <grsruggirello25@gmail.com>
2021-10-27 11:36:13 -0400

    Added MergeSort Algorithm
    Completed Merge Sort Algorithm for Arrays/Lists

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -4,20 +4,41 @@
 import random
 
 #---Functions---#
-def mergesort(arrayList):
-   print (len(arrayList)) #size of array
-   left = arrayList[0] #first value of array
-   right = arrayList[-1] #last value of array
+def MergeSort(arrayList): #Runs the Merge Sort Algorithm Functions
 
-   print(left, right)
+   if len(arrayList) > 1:
+      half_array = int(len(arrayList)*0.5) #half the length of the array
+   
+      left = arrayList[:half_array] #left split of array
+      right = arrayList[half_array:] #right half of array
 
+      MergeSort(left)
+      MergeSort(right)
+      
+      i = j = k = 0 #establishing loop variables
+
+      while i < len(left) and j < len(right): #Sorting loop with two halves
+         if left[i] < right[j]:
+            arrayList[k] = left[i]
+            i+=1
+      
+         else:
+            arrayList[k] = right[j]
+            j+=1
+      
+         k+=1
+
+   
 #----Main Code----#
 
-random.seed(0)
+random.seed(0) #creates random seed
 
-B = [random.randint(0,10**7) for i in range(2**20)]
+B = [random.randint(0,10**7) for i in range(2**20)] #generates random numbers
 
-print (B[0:5])
-print (B[10000:10005])
+print (B[0:5]) #prints inital order of first 5 values
+print (B[10000:10005]) #prins inital order of values 10000 to 10005
 
-mergesort(B)
+MergeSort(B) #runs merge sort algorithm
+
+print (B[0:5]) #should be first 5 smallest values
+print (B[10000:10005]) #should be 5 different values
